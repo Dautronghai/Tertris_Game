9e0679c5e78434b26f2d28c67e8fc8de6c383812
Nguyenhai <nguyenhai.nxo@gmail.com>
2020-03-08 15:03:19 +0700

    add function listener key board

--- dot.js
+++ dot.js
@@ -11,9 +11,52 @@ class dot{
 		let r = this.row * this.size;
 		let c = this.col * this.size;
 		this.context.strokeStyle = this.color;
-		this.context.strokeRect(c + 1, r + 1,this.size-2,this.size -2 );		
+		this.context.strokeRect(c + 2, r + 2,this.size - 3,this.size - 3);		
 		this.context.stroke();
 		this.context.fillStyle = this.color;
-		this.context.fillRect(c + 4, r + 4, this.size - 8, this.size - 8);
+		this.context.fillRect(c + 4, r + 4, this.size - 7, this.size - 7);
+	}
+	//if dot move to border right
+	hitRight(){
+		let hit_right = false;
+		if(this.col == NUMCOLS - 1){
+			hit_right = true;
+		}
+		return hit_right;
+	}
+	//update position colum when dot move to right
+	moveRight(){	
+		if(!this.hitRight()){
+			this.col++;
+		}
+	}
+	//if dot move to border left
+	hitLeft(){
+		let hit_left = false;
+		if(this.col == 0){
+			hit_left = true;
+		}
+		return hit_left;
+	}
+	//update position colum dot move to left
+	moveLeft(){	
+		if(!this.hitLeft()){
+			this.col--;
+		}
+	}
+	//if dot move to bottom border
+	hitBottom(){
+		let hit_bottom = false;
+		if(this.row == NUMROWS - 1){
+			hit_bottom = true;
+		}
+		return hit_bottom;
+	}
+	//update positon colum dot when move up
+	fall(){	
+		if(!this.hitBottom()){
+			this.row++;
+		}
+		
 	}
 }
--- game.js
+++ game.js
@@ -1,8 +1,10 @@
+const NUMROWS = 20;
+const NUMCOLS = 10;
 class game{
 	constructor(){
 		this.canvas = null;
 		this.context = null;
-		this.init();		
+		this.init();				
 		this.loop();
 	}
 	init(){
@@ -13,24 +15,44 @@ class game{
 		this.canvas.height = 400;
 		// append canvas to page
 		document.body.appendChild(this.canvas);
-		this.board = new board(this);
-		this.d = new dot(this,0,0,"000000");
+		this.board = new board(this);		
 		this.startGame();
+		this.d = new dot(this,0,0,"#000000");
+		this.listenKeyboard();
 	}
 	clearScreen(){
 		this.board.backgroundBoard();
 		//this.context.fillStyle = "#000000";
 		//this.context.fillRect(0, 0, 200, 400);
 	}	
+	//listen event key press for control
+	listenKeyboard(){
+		document.addEventListener('keydown',(event)=>{			
+			switch(event.key){
+				case 'ArrowRight':				
+					this.d.moveRight();
+				break;
+				case 'ArrowLeft':
+					this.d.moveLeft();
+				break;
+				case 'ArrowUp':
+				break;
+				case 'ArrowDown':
+				break;
+				}
+		
+		});
+	}
 	//startgame
 	//brick fall 1 row per secon
 	startGame(){
 		return setInterval(()=>{
+			this.d.fall();
 		},1000);
 	}
 	draw(){
 		//clear before draw object
-		this.clearScreen();		
+		this.clearScreen();				
 		//draw object
 		this.board.draw();
 		this.d.draw();
@@ -39,7 +61,8 @@ class game{
 	//create loop for game
 	//30% secon draw again
 	loop(){
-		this.draw();				
+		this.draw();	
+		this.d.draw();		
 		setTimeout(()=>this.loop(),30);
 	}
 	
